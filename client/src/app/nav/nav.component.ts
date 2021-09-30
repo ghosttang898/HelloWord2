18d2b4a8a292aac8aad47c7dcc86bdce64376a1f
ghosttang <ghosttang@gmail.com>
2021-09-29 21:10:01 -0400

    End of # S7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -27,10 +27,11 @@ export class NavComponent implements OnInit {
       this.router.navigateByUrl('/members');
       // console.log(response);
       // this.loggedIn = true;
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
     })
+    // , error => {
+    //   console.log(error);
+    //   this.toastr.error(error.error);
+    // }
   }
 
   logout() {
