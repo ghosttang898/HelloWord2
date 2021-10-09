a40cc6234c613ab118ddd25d196cb026b2c2fdec
ghosttang <ghosttang@gmail.com>
2021-10-09 18:05:50 -0400

    End of S #8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,36 +2,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-
+    [Authorize]
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
+        private readonly IUserRepository _userRespository;
+        private readonly IMapper _mapper;
 
-        public UsersController(DataContext context)
+        public UsersController(IUserRepository userRespository, IMapper mapper)
         {
-            _context = context;
+            _userRespository = userRespository;
+            _mapper = mapper;
         }
 
         [HttpGet]
-        [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        // [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            return await _context.Users.ToListAsync();
+            //return Ok(await _userRespository.GetUsersAsync());
+            var users = await _userRespository.GetMembersAsync();
+
+            //var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+
+            return Ok(users);
         }
 
         // api/users/{id}
-        [Authorize]
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id)
+        // [Authorize]
+        // [HttpGet("{id}")]
+        // public async Task<ActionResult<MemberDto>> GetUser(int id)
+        // {
+        //     var user = await _userRespository.GetUserByIdAsync(id);
+
+        //     return _mapper.Map<MemberDto>(user);
+        // }
+
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            return await _context.Users.FindAsync(id);
+            // var user = await _userRespository.GetUserByUsernameAsync(username);
+
+            // return _mapper.Map<MemberDto>(user);
+
+            return await _userRespository.GetMemberAsync(username);
         }
     }
 }
---
+++ API/Data/Migrations/20211009021302_ExtendedUserEntity_fix.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class ExtendedUserEntity_fix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateOfBrith",
+                table: "Users",
+                newName: "DateOfBirth");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateOfBirth",
+                table: "Users",
+                newName: "DateOfBrith");
+        }
+    }
+}
